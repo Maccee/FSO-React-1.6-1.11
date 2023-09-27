7d15ed7b083b736a9fcef841ea3ea30580000ba3
Macce <makkeeh@gmail.com>
2023-09-27 13:30:04 +0300

    1.8 props

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,17 @@ const Button = (props) => {
   return <button onClick={props.handleClick}>{props.text}</button>;
 };
 
-const Statistics = ({ good, neutral, bad }) => {
-  const total = good + neutral + bad;
-  const average = (good - bad) / total;
-  const positive = (100 / total) * good;
+const Statistics = (props) => {
+  const total = props.good + props.neutral + props.bad;
+  const average = (props.good - props.bad) / total;
+  const positive = (100 / total) * props.good;
 
   return (
     <div>
       <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
+      <p>good {props.good}</p>
+      <p>neutral {props.neutral}</p>
+      <p>bad {props.bad}</p>
       <p>all {total}</p>
       <p>average {average}</p>
       <p>positive {positive} %</p>
