6e1c63373f807664f36275ab2bdd6a3db6c50b4f
Macce <makkeeh@gmail.com>
2023-09-27 14:11:03 +0300

    1.11

--- src/App.jsx
+++ src/App.jsx
@@ -22,12 +22,14 @@ const Statistics = (props) => {
   return (
     <div>
       <h1>statistics</h1>
-      <StatisticLine text="good" value={props.good} />
-      <StatisticLine text="neutral" value={props.neutral} />
-      <StatisticLine text="bad" value={props.bad} />
-      <StatisticLine text="all" value={total} />
-      <StatisticLine text="average" value={average} />
-      <StatisticLine text="positive" value={positive} />
+      <table>
+        <StatisticLine text="good" value={props.good} />
+        <StatisticLine text="neutral" value={props.neutral} />
+        <StatisticLine text="bad" value={props.bad} />
+        <StatisticLine text="all" value={total} />
+        <StatisticLine text="average" value={average} />
+        <StatisticLine text="positive" value={positive} />
+      </table>
     </div>
   );
 };
@@ -35,15 +37,17 @@ const Statistics = (props) => {
 const StatisticLine = (props) => {
   if (props.text === "positive") {
     return (
-      <p>
-        {props.text} {props.value} %
-      </p>
+      <tr>
+        <td>{props.text}</td>
+        <td>{props.value} %</td>
+      </tr>
     );
   }
   return (
-    <p>
-      {props.text} {props.value}
-    </p>
+    <tr>
+      <td>{props.text}</td>
+      <td>{props.value}</td>
+    </tr>
   );
 };
 
